137feac15ca9b94f071aceeee5827fce1fabac9a
Daniel Nascimento <daniel.conceicao@einstein.br>
2026-01-06 08:49:53 -0300

    feat: move cloud run mod call to main

--- cloudrun.tf
+++
@@ -1,8 +0,0 @@
-module "cloudrun" {
-  source = "./modules/cloudrun"
-
-  service_name = "alianca"
-  location     = "us-central1"
-  project_id   = "alianca-solar"
-  image        = "us-central1-docker.pkg.dev/alianca-solar/cloud-run-source-deploy/alianca@sha256:7ea696b312747133d64aeba7f1b77d677a698569475e92473a2fb7e7e4f0a0c2"
-}
